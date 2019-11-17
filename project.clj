eee6232bad8ae02521e2014513685d1220698518
Peter Fraenkel <pnf@podsnap.com>
2019-11-17 18:32:51 -0500

    Use graal ce

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [incanter "1.9.3"]
-                 ;;[net.mikera/core.matrix "0.62.0"]
-                 [uncomplicate/neanderthal "0.26.1"]
+                 [net.mikera/core.matrix "0.62.0"]
+                 ;; [uncomplicate/neanderthal "0.26.1"]
                  [org.clojure/core.async "0.4.500"]]
   :repl-options {:init-ns memuse.core}
   ;; Oracle 12
@@ -16,10 +16,12 @@
   ;; :jvm-opts ["-XX:+UnlockExperimentalVMOptions" "-XX:+UseShenandoahGC"]
   ;; J9
   ;; :java-cmd "/Users/pnf/dist/jdk-12.0.2+10/Contents/Home/bin/java"
-  :java-cmd "/home/pnf/dist/jdk-12.0.2+10/bin/java"
-  :jvm-opts ["-Xgc:concurrentScavenge" "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"]
-  :plugins [[lein-with-env-vars "0.2.0"]]
-  :hooks [leiningen.with-env-vars/auto-inject]  ;; works for now
-  :env-vars {:LD_LIBRARY_PATH "/opt/intel/compilers_and_libraries/linux/mkl/lib/intel64:/opt/intel/lib"}
+  ;; :java-cmd "/home/pnf/dist/jdk-12.0.2+10/bin/java"
+  :java-cmd "/home/pnf/dist/graalvm-ce-19.2.1/bin/java"
+  ;; :jvm-opts ["-Xgc:concurrentScavenge" ]
+  ;; "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"
+  ;; :plugins [[lein-with-env-vars "0.2.0"]]
+  ;; :hooks [leiningen.with-env-vars/auto-inject]  ;; works for now
+  ;; :env-vars {:LD_LIBRARY_PATH "/opt/intel/compilers_and_libraries/linux/mkl/lib/intel64:/opt/intel/lib"}
 
   )
